db0bb34aa4e8b9a92d0f6a672411339a777a049b
Darshan Chudiwal <darshan.chudiwal@fareportal.com>
2022-02-15 23:26:18 +0530

    first commit

---
+++ Activity1/Employee.js
@@ -0,0 +1,69 @@
+function EmployeeDetails() {
+  var name= "Darshan";
+  var age = 21;
+  var salary = 10000;
+  var id = Date.now();
+  var jobdesc = "SDE";
+  var level = "1";
+  var managerName = "Shivam";
+  const calculateIncrement = function(amount) {
+    salary = salary + amount;
+      return salary;
+  }
+  const changeManager = function(newManager) {
+    managerName = newManager;
+  }
+
+  const getAge = function() {
+    return age;
+  }
+
+  const getId = function() {
+      return id;
+  }
+  const getName = function() {
+    return name;
+  } 
+
+  const getDesignation = function() {
+    return jobdesc+level;
+  }
+
+  const changeLevel = function(newLevel) {
+    level = newLevel;
+  }
+
+  const changeJobDesc = function(newDesc) {
+    jobdesc = newDesc;
+  }
+
+  const setManagerName = function(manager) {
+    managerName = manager;   
+  }
+
+  const getManagerName = function() {
+      return managerName;
+  }
+  return {
+    calculateIncrement: calculateIncrement,
+    getId : getId,
+    getName : getName,
+      getAge: getAge,
+    getDesignation : getDesignation,
+      getManagerName:getManagerName,
+    changeLevel : changeLevel,
+    changeJobDesc : changeJobDesc,
+      setManagerName : setManagerName
+  }
+}
+
+var newEmployee = EmployeeDetails()
+
+console.log(newEmployee.getName());
+console.log(newEmployee.getAge());
+console.log(newEmployee.getDesignation());
+console.log(newEmployee.getManagerName());
+console.log(newEmployee.getId());
+console.log(newEmployee.calculateIncrement(1000));
+
+
